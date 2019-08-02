71b104151140a4248ba1d5cd79ff0d2f64525c30
Zooheck <davidscottflack@gmail.com>
2019-08-01 19:35:13 -0500

    Layout movie cards on grid

--- flackflix/src/components/MovieCard/index.js
+++ flackflix/src/components/MovieCard/index.js
@@ -12,8 +12,10 @@ const MovieCard = props => {
   } = props.movie;
   const genreList = genre_ids.map(id => genres[id]);
   return (
-    <div>
-      <img src={`${posterUrl}/${poster_path}`} alt={`Poster for ${title}.`} />
+    <div className="movie-card">
+      <div className="card-img-container">
+        <img src={`${posterUrl}/${poster_path}`} alt={`Poster for ${title}.`} />
+      </div>
       <h2>{title}</h2>
       <h3>{vote_average} user score</h3>
       <h3>Release Date: {release_date}</h3>
