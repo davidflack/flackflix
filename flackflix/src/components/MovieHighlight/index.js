2ba9b7c165d2b4717465594036b7cfbd8c80b1f4
Zooheck <davidscottflack@gmail.com>
2019-08-02 10:36:40 -0500

    Add details to MovieHighlight

--- flackflix/src/components/MovieHighlight/index.js
+++ flackflix/src/components/MovieHighlight/index.js
@@ -1,7 +1,39 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { genres, posterUrl, backdropUrl } from "../../variables";
 const MovieHighlight = props => {
-  return <div>Highlight for {props.movie.title}</div>;
+  const {
+    id,
+    title,
+    backdrop_path,
+    poster_path,
+    overview,
+    genre_ids,
+    release_date,
+    vote_average
+  } = props.movie;
+  const genreList = genre_ids.map(id => genres[id]);
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div>
+        <p>{vote_average}/10</p>
+        <p>{release_date}</p>
+      </div>
+      <ul>
+        {genreList.map(genre => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+      <p>{overview}</p>
+      <Link to={`/movie/${id}`}>More Details</Link>
+      <img src={`${posterUrl}/${poster_path}`} alt={`Poster for ${title}.`} />
+      <img
+        src={`${backdropUrl}/${backdrop_path}`}
+        alt={`Backdrop for ${title}.`}
+      />
+    </div>
+  );
 };
 
 export default MovieHighlight;
