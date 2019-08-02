8eb8df8bb7b91aef5f62879d7cb4f3e0608f8b4c
Zooheck <davidscottflack@gmail.com>
2019-08-02 17:50:11 -0500

    Add blurry background movie highlight poster

--- flackflix/src/components/MovieHighlight/index.js
+++ flackflix/src/components/MovieHighlight/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { genres, posterUrl, backdropUrl } from "../../variables";
+
 const MovieHighlight = props => {
   const {
     id,
     title,
     backdrop_path,
-    poster_path,
     overview,
     genre_ids,
     release_date,
@@ -14,24 +14,26 @@ const MovieHighlight = props => {
   } = props.movie;
   const genreList = genre_ids.map(id => genres[id]);
   return (
-    <div>
-      <h2>{title}</h2>
-      <div>
-        <p>{vote_average}/10</p>
-        <p>{release_date}</p>
+    <div
+      style={{
+        background: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${backdropUrl}/${backdrop_path})`
+      }}
+      className="movie-highlight"
+    >
+      <div className="highlight-info">
+        <h2 className="highlight-title">{title}</h2>
+        <div className="highlight-date-vote">
+          <p>{release_date}</p>
+          <p>{vote_average}/10</p>
+        </div>
+        <ul>
+          {genreList.map(genre => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+        <p>{overview}</p>
+        <Link to={`/movie/${id}`}>More Details</Link>
       </div>
-      <ul>
-        {genreList.map(genre => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
-      <p>{overview}</p>
-      <Link to={`/movie/${id}`}>More Details</Link>
-      <img src={`${posterUrl}/${poster_path}`} alt={`Poster for ${title}.`} />
-      <img
-        src={`${backdropUrl}/${backdrop_path}`}
-        alt={`Backdrop for ${title}.`}
-      />
     </div>
   );
 };
