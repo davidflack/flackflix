ecc67490249fd410cede4c2f1e7cc4246c97e12f
Zooheck <davidscottflack@gmail.com>
2019-08-03 20:27:09 -0500

    Add hover on movie cards.

--- flackflix/src/components/MovieCard/index.js
+++ flackflix/src/components/MovieCard/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import { genres, posterUrl } from "../../variables";
 const MovieCard = props => {
   const {
@@ -12,21 +12,27 @@ const MovieCard = props => {
   } = props.movie;
   const genreList = genre_ids.map(id => genres[id]);
   return (
-    <div className="movie-card">
+    <div
+      className="movie-card"
+      onClick={() => props.history.push(`/movie/${id}`)}
+    >
       <div className="card-img-container">
         <img src={`${posterUrl}/${poster_path}`} alt={`Poster for ${title}.`} />
       </div>
-      <h2>{title}</h2>
-      <h3>{vote_average} user score</h3>
+      <div className="card-text-container">
+        <h2>{title.slice(0, 20)}</h2>
+        <h2>{release_date.slice(0, 4)}</h2>
+      </div>
+      {/* <h3>{vote_average} user score</h3>
       <h3>Release Date: {release_date}</h3>
       <ul>
         {genreList.map(genre => (
           <li key={genre}>{genre}</li>
         ))}
       </ul>
-      <Link to={`/movie/${id}`}>More Details</Link>
+      <Link to={`/movie/${id}`}>More Details</Link> */}
     </div>
   );
 };
 
-export default MovieCard;
+export default withRouter(MovieCard);
