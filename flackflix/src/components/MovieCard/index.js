5fd99b9a154a3a87a96bf0e4a57dd9366c6f0cc8
Zooheck <davidscottflack@gmail.com>
2019-08-06 15:42:28 -0500

    Remove text from movie card

--- flackflix/src/components/MovieCard/index.js
+++ flackflix/src/components/MovieCard/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import { posterUrl } from "../../variables";
 const MovieCard = props => {
-  const { title, release_date, poster_path, id } = props.movie;
+  const { title, poster_path, id } = props.movie;
   return (
     <div
       className="movie-card"
@@ -10,10 +10,6 @@ const MovieCard = props => {
     >
       <div className="card-img-container">
         <img src={`${posterUrl}/${poster_path}`} alt={`Poster for ${title}.`} />
-      </div>
-      <div className="card-text-container">
-        <h2>{title.slice(0, 20)}</h2>
-        <h2>{release_date.slice(0, 4)}</h2>
       </div>
     </div>
   );
