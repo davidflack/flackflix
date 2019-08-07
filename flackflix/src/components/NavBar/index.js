06598a7ed9317535baa26910a510ad32f3825677
Zooheck <davidscottflack@gmail.com>
2019-08-07 17:04:11 -0500

    Add toggling mobile searchbar

--- flackflix/src/components/NavBar/index.js
+++ flackflix/src/components/NavBar/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { NavLink, withRouter } from "react-router-dom";
 const NavBar = props => {
   const [searchText, setSearchText] = useState("");
-  const [browseBtn, setBrowseBtn] = useState(false);
+  const [mobileSearch, setMobileSearch] = useState(false);
   const [browseMenu, setBrowseMenu] = useState(false);
   const handleChange = e => {
     e.preventDefault();
@@ -15,27 +15,50 @@ const NavBar = props => {
   };
   return (
     <>
-      <div className="navbar-container">
-        <h1 className="logo">FlackFlix</h1>
-        <div className="nav-link-container">
-          <NavLink to="/popular">Popular</NavLink>
-          <NavLink to="/top-rated">Top Rated</NavLink>
-          <NavLink to="/now-playing">Now Playing</NavLink>
-        </div>
-        <div className="mobile-browse-button">
-          <p onClick={() => setBrowseMenu(!browseMenu)}>
-            Browse <i class="fas fa-sort-down" />
-          </p>
-        </div>
-        <form className="searchbar-container" onSubmit={search}>
-          <input
-            type="text"
-            name="searchText"
-            value={searchText}
-            onChange={handleChange}
-            placeholder="Search Movies"
+      <div>
+        <div className="navbar-container">
+          <h1 className="logo">FlackFlix</h1>
+          <div className="nav-link-container">
+            <NavLink to="/popular">Popular</NavLink>
+            <NavLink to="/top-rated">Top Rated</NavLink>
+            <NavLink to="/now-playing">Now Playing</NavLink>
+          </div>
+          <div className="mobile-browse-button">
+            <p onClick={() => setBrowseMenu(!browseMenu)}>
+              Browse <i className="fas fa-sort-down" />
+            </p>
+          </div>
+          <form className="searchbar-container" onSubmit={search}>
+            <input
+              type="text"
+              name="searchText"
+              value={searchText}
+              onChange={handleChange}
+              placeholder="Search Movies"
+            />
+          </form>
+
+          <form
+            className={
+              mobileSearch
+                ? "mobile-searchbar-container"
+                : "mobile-searchbar-container-off"
+            }
+            onSubmit={search}
+          >
+            <input
+              type="text"
+              name="searchText"
+              value={searchText}
+              onChange={handleChange}
+              placeholder="Search Movies"
+            />
+          </form>
+          <i
+            className={`fas ${mobileSearch ? "fa-times" : "fa-search"} fa-2x`}
+            onClick={() => setMobileSearch(!mobileSearch)}
           />
-        </form>
+        </div>
       </div>
       <div
         className={
