9efcb971d190e6dc1a0fd82a69e09f598f4d97c7
Zooheck <davidscottflack@gmail.com>
2019-08-07 14:56:39 -0500

    Implement togglable browse button on mobile

--- flackflix/src/components/NavBar/index.js
+++ flackflix/src/components/NavBar/index.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { NavLink, withRouter } from "react-router-dom";
 const NavBar = props => {
   const [searchText, setSearchText] = useState("");
+  const [browseBtn, setBrowseBtn] = useState(false);
+  const [browseMenu, setBrowseMenu] = useState(false);
   const handleChange = e => {
     e.preventDefault();
     setSearchText(e.target.value);
@@ -12,23 +14,47 @@ const NavBar = props => {
     setSearchText("");
   };
   return (
-    <div className="navbar-container">
-      <h1 className="logo">FlackFlix</h1>
-      <div className="nav-link-container">
-        <NavLink to="/popular">Popular</NavLink>
-        <NavLink to="/top-rated">Top Rated</NavLink>
-        <NavLink to="/now-playing">Now Playing</NavLink>
+    <>
+      <div className="navbar-container">
+        <h1 className="logo">FlackFlix</h1>
+        <div className="nav-link-container">
+          <NavLink to="/popular">Popular</NavLink>
+          <NavLink to="/top-rated">Top Rated</NavLink>
+          <NavLink to="/now-playing">Now Playing</NavLink>
+        </div>
+        <div className="mobile-browse-button">
+          <p onClick={() => setBrowseMenu(!browseMenu)}>
+            Browse <i class="fas fa-sort-down" />
+          </p>
+        </div>
+        <form className="searchbar-container" onSubmit={search}>
+          <input
+            type="text"
+            name="searchText"
+            value={searchText}
+            onChange={handleChange}
+            placeholder="Search Movies"
+          />
+        </form>
       </div>
-      <form className="searchbar-container" onSubmit={search}>
-        <input
-          type="text"
-          name="searchText"
-          value={searchText}
-          onChange={handleChange}
-          placeholder="Search Movies"
-        />
-      </form>
-    </div>
+      <div
+        className={
+          browseMenu
+            ? "mobile-nav-link-container"
+            : "mobile-nav-link-container-off"
+        }
+      >
+        <NavLink onClick={() => setBrowseMenu(!browseMenu)} to="/popular">
+          Popular
+        </NavLink>
+        <NavLink onClick={() => setBrowseMenu(!browseMenu)} to="/top-rated">
+          Top Rated
+        </NavLink>
+        <NavLink onClick={() => setBrowseMenu(!browseMenu)} to="/now-playing">
+          Now Playing
+        </NavLink>
+      </div>
+    </>
   );
 };
 
