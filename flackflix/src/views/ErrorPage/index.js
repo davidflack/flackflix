e51ba2be09b9342da049e6fafae55b72b36af162
Zooheck <davidscottflack@gmail.com>
2019-08-11 16:22:23 -0500

    Style error page.

--- flackflix/src/views/ErrorPage/index.js
+++ flackflix/src/views/ErrorPage/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Error Page</h1>
+    <div className="error-page-container">
+      <h2>Uh-Oh!</h2>
+      <h3>The page you are looking for does not exist.</h3>
+      <h3>
+        <Link to="/popular" className="redirect-link">
+          Click here
+        </Link>{" "}
+        to browse popular movies.
+      </h3>
     </div>
   );
 };
