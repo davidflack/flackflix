b819ed8065d3c0f408263a7a863d2d3dcdd79b0e
Zooheck <davidscottflack@gmail.com>
2019-08-13 12:37:10 -0500

    Fix react router scrolling bug

--- flackflix/src/hooks/useFetch.js
+++ flackflix/src/hooks/useFetch.js
@@ -1,11 +1,21 @@
 import { useState, useEffect } from "react";
 
+// useFetch is a custom hook used for fetching data from an external api when a component renders.
+
+// It takes a url and a dependency array as parameters. When the component renders, useFetch will
+// make a GET request to the given url. It will make another GET request whenever an item in the dependency
+// array changes.
+
+// useFetch returns a loading boolean, the data from the API, and an error boolean.
+
 export const useFetch = (url, dependencies) => {
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [data, setData] = useState(null);
   useEffect(() => {
     setLoading(true);
+    // fix react-router scroll bug, automatically scroll to top of page
+    window.scrollTo(0, 0);
     fetch(url)
       .then(response => {
         if (!response.ok) {
